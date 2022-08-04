07bfd5af30eda53aa932712759d0f25073026976
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-08-04 20:28:06 -0300

    feat: first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    printf("Hello world!\n");
+    printf("Hello World 2\n");
+
+
+    // using variables
+    char firstVariable[] = "Teste";
+    int number = 12;
+
+    printf("firstVarible: %s\n", firstVariable);
+    printf("number: %d\n", number);
+    return 0;
+}
