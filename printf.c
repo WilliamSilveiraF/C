f76d4ca5feaf86bd712243f7ce9c054592811a8e
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-08-10 19:47:44 -0300

    feat: printf

---
+++ printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Printing ex: ");
+    printf("Printing a char: %c\n", 'Z');
+    printf("Printing a number: %d\n", 8);
+    printf("Printing a float: %f\n", 9.1);
+    printf("Printing string: %s\n", "my string");
+}
